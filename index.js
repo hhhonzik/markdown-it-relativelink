568508a15b229855e7ee70df89a0bca365b5bdbf
Honza2 <honza@tapdaq.com>
2015-10-26 14:01:57 +0100

    Add support for anchors and custom captions

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ var plugin = require('markdown-it-regexp');
 module.exports = function (opts) {
   return plugin(
     // regexp to match
-    /\[\[([a-zA-Z0-9_ ]+)\]\]/,
+    /\[\[([\|\-\#a-zA-Z0-9_ ]+)\]\]/,
 
     // this function will be called when something matches
     function(match, utils) {
-      var url = opts.prefix + match[1].replace(' ', '-');
-      var caption = utils.escape(match[1]);
+      var text = match[1];
+      var caption, url;
+
+      // do we have a label?
+      if (text.indexOf('|') !== -1) {
+        caption = utils.escape(text.split('|')[0]);
+        url = opts.prefix + text.split('|')[1].replace(' ', '-');
+      } else {
+        url = opts.prefix + text.replace(' ', '-');
+        caption = utils.escape(match[1]);
+      }
+      // URL#anchor -> URL in caption
+      if (caption.indexOf('#') !== -1) {
+        caption = caption.slice(0, caption.indexOf('#'));
+      }
 
       return '<a href="' + utils.escape(url) + '">'
            + caption
